f64fb82bb84a21293c33be979ee55ac7049a601d
Gilberto Mascena <gilbertomascena@gmail.com>
2024-08-08 20:23:32 -0300

    created readme.md and added swagger documentation

--- src/test/java/br/com/mascenadev/screanmatchfrases/ScreanmatchFrasesApplicationTests.java
+++ src/test/java/br/com/mascenadev/screanmatchfrases/ScreanmatchFrasesApplicationTests.java
@@ -3,11 +3,10 @@ package br.com.mascenadev.screanmatchfrases;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest()
 class ScreanmatchFrasesApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
