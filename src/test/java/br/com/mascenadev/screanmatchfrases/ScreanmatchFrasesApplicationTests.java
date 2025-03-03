89ef706a6e12949e7b6b670d4f1541b2e2671a4d
Gilberto Mascena <gilbertomascena@gmail.com>
2025-03-03 17:56:40 -0300

    added images

---
+++ src/test/java/br/com/mascenadev/screanmatchfrases/ScreanmatchFrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mascenadev.screanmatchfrases;
+
+import br.com.mascenadev.screanmatchfrases.dto.PhraseDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest()
+class ScreanmatchFrasesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
