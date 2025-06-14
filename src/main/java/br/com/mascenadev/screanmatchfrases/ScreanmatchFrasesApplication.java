7d834bf828cfd95a950006e547ddecc69248cdf5
Gilberto Mascena <gilbertomascena@gmail.com>
2025-06-14 15:35:34 -0300

    :recycle: refactor: class ScreanmatchFrasesApplication

--- src/main/java/br/com/mascenadev/screanmatchfrases/ScreanmatchFrasesApplication.java
+++ src/main/java/br/com/mascenadev/screanmatchfrases/ScreanmatchFrasesApplication.java
@@ -2,10 +2,9 @@ package br.com.mascenadev.screanmatchfrases;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@Profile("test")
+
 public class ScreanmatchFrasesApplication {
 
     public static void main(String[] args) {
