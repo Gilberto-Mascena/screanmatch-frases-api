9ff543b34a3d2da3fe64af7d0d24c99bdfd765eb
Gilberto Mascena <gilbertomascena@gmail.com>
2025-06-14 15:50:14 -0300

    :recycle: refactor: class ScreanmatchFrasesApplicationTests

--- src/test/java/br/com/mascenadev/screanmatchfrases/ScreanmatchFrasesApplicationTests.java
+++ src/test/java/br/com/mascenadev/screanmatchfrases/ScreanmatchFrasesApplicationTests.java
@@ -1,6 +1,5 @@
 package br.com.mascenadev.screanmatchfrases;
 
-import br.com.mascenadev.screanmatchfrases.dto.PhraseDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
