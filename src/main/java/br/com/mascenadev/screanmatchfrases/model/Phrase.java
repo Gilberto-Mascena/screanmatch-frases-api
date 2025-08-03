a4828c1d4554b410efca19cb49d95d08f2e5896b
Gilberto Mascena <gilbertomascena@gmail.com>
2025-08-03 14:07:04 -0300

    :recycle: refactor: Updating imports in the Phrase class

--- src/main/java/br/com/mascenadev/screanmatchfrases/model/Phrase.java
+++ src/main/java/br/com/mascenadev/screanmatchfrases/model/Phrase.java
@@ -1,6 +1,10 @@
 package br.com.mascenadev.screanmatchfrases.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * @author Gilberto Dev
